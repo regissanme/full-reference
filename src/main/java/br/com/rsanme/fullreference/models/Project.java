5a6f4b02e739e894fae3d4641d8af7596e981db8
regissanme <rsanme@gmail.com>
2023-11-13 11:41:20 -0300

    create Project class

---
+++ src/main/java/br/com/rsanme/fullreference/models/Project.java
@@ -0,0 +1,49 @@
+package br.com.rsanme.fullreference.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.OffsetDateTime;
+import java.util.Objects;
+
+/**
+ * Projeto: full-reference
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 13/11/2023
+ * Hora: 11:34
+ */
+@Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor @ToString
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome do projeto é obrigatório!")
+    @Column(unique = true, nullable = false)
+    private String name;
+    private String description;
+
+    @CreatedDate
+    private OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    private OffsetDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Project project = (Project) o;
+        return Objects.equals(id, project.id) && Objects.equals(name, project.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
