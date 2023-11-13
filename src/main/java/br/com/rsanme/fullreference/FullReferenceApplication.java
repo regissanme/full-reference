2bf0deee87db27a4ee63dc4a5a59ecb1a42dfb5f
regissanme <rsanme@gmail.com>
2023-11-13 11:06:25 -0300

    configure jpa properties

---
+++ src/main/java/br/com/rsanme/fullreference/FullReferenceApplication.java
@@ -0,0 +1,13 @@
+package br.com.rsanme.fullreference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullReferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullReferenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rsanme/fullreference/FullReferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rsanme.fullreference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullReferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
