407f2146f9c361e85425b271d5d74232634bb84c
regissanme <rsanme@gmail.com>
2023-11-13 18:01:17 -0300

    create Task class

---
+++ src/main/java/br/com/rsanme/fullreference/models/Task.java
@@ -0,0 +1,59 @@
+package br.com.rsanme.fullreference.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.OffsetDateTime;
+import java.util.Objects;
+
+/**
+ * Projeto: full-reference
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 13/11/2023
+ * Hora: 11:41
+ */
+@Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor @ToString
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome da tarefa é obrigatório!")
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    private String notes;
+
+    private boolean isCompleted;
+
+    private OffsetDateTime deadline;
+
+    @CreatedDate
+    private OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    private OffsetDateTime updatedAt;
+
+    @ManyToOne
+    private Project project;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
