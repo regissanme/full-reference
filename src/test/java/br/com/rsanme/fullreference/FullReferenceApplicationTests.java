3d8f0c91112bd859bbfb9c39081bfea8c88f53f2
regissanme <rsanme@gmail.com>
2023-11-13 22:40:57 -0300

    test: create test for context load main

--- src/test/java/br/com/rsanme/fullreference/FullReferenceApplicationTests.java
+++ src/test/java/br/com/rsanme/fullreference/FullReferenceApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FullReferenceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void whenStartApplicationTheContextLoads() {
+        FullReferenceApplication.main(new String[]{});
+    }
 
 }
