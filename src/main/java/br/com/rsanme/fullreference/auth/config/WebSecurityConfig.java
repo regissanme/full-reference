6795fa38b842b2aec2e6ce62dc2a312f7fcf7efa
regissanme <rsanme@gmail.com>
2023-11-21 16:17:24 -0300

    refactor: add security filter chain rules

--- src/main/java/br/com/rsanme/fullreference/auth/config/WebSecurityConfig.java
+++ src/main/java/br/com/rsanme/fullreference/auth/config/WebSecurityConfig.java
@@ -1,8 +1,19 @@
 package br.com.rsanme.fullreference.auth.config;
 
+import br.com.rsanme.fullreference.auth.jwt.TokenFilter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * Projeto: full-reference
@@ -10,10 +21,40 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * Data: 21/11/2023
  * Hora: 15:14
  */
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
+
+    private final TokenFilter tokenFilter;
+
+    public WebSecurityConfig(TokenFilter tokenFilter) {
+        this.tokenFilter = tokenFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers("/project/**").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers("/task/**").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
